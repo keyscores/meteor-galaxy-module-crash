1f3a7502ca6bf6d2aa2b4059c830afc4613ce67f
Lucas Geiger <LUCAS@Lucass-MacBook-Air.local>
2016-10-07 12:27:25 -0400

    test method

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+// import { ReactiveVar } from 'meteor/reactive-var';
+import { Session } from 'meteor/session';
 
 import './main.html';
 
@@ -7,6 +8,17 @@ import './main.html';
 Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
-    Meteor.call('runSandbox')
+    Meteor.call('runSandbox', function(err, res){
+      var array = Session.get('methodResponse', res) || []
+      array.push( res )
+      Session.set('methodResponse', array)
+    })
   },
+});
+
+Template.hello.helpers({
+  methodResponse: function(){
+    return JSON.stringify( Session.get('methodResponse') );
+
+  }
 });
--- server/main.js
+++ server/main.js
@@ -1,23 +1,69 @@
 import { Meteor } from 'meteor/meteor';
+import Future from 'fibers/future';
+
+
 const SANDBOX = require('vQMgArq1o4U1').sandbox;
+const startCluster = require('vQMgArq1o4U1').startCluster
+const sendScriptJob = require('vQMgArq1o4U1').sendScriptJob
+
 
 
 Meteor.startup(() => {
   // code to run on server at startup
+  startCluster(function(){
+    console.log('cluster started');
+  })
 });
 
 
 Meteor.methods({
-  runSandbox :function(){
+  runLegacy :function(){
+
+    var fut = new Future();
+
 
     sandboxed_code = 'function botFunction(){ return "a"}; RESULT = botFunction()'
     SANDBOX( sandboxed_code , [] , {} , function(err, res ){
       if (err ){
         console.log('err: ', err);
+        fut.return(err);
+
 
       }else{
         console.log('res: ', res);
+        fut.return(res);
+
       }
     })
+
+    return fut.wait();
+
+  },
+  runSandbox: function(){
+    sandboxed_code = 'function botFunction(){ return "a"}; RESULT = botFunction()'
+
+    var scriptObj = {
+      script : sandboxed_code,
+      libs: [],
+      context: {}
+    }
+
+    // sendScriptJob( {  script : 'wrapper=function(){ return 1+8 }; RESULT = wrapper()'}, function(err, res ){})
+    var fut = new Future();
+
+    sendScriptJob( scriptObj, function(err, res ){
+      if (err){
+        console.log('err: ', err);
+        fut.return(err);
+
+      }
+      if (res){
+        console.log('res: ', res);
+        fut.return(res);
+
+      }
+    });
+
+    return fut.wait();
   }
 });
